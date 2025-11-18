b3866063645e098dc4bc97fcc9cbba9db041583a
le <lexu.xu337@gmail.com>
2025-11-18 13:54:48 -0500

    feat: add partial implementation of DataManager

--- app/src/main/java/com/example/smartair/DataManager.java
+++ app/src/main/java/com/example/smartair/DataManager.java
@@ -1,7 +1,22 @@
 package com.example.smartair;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class DataManager {
 
+    FirebaseDatabase database;
 
+    public DataManager() {
+        database = FirebaseDatabase.getInstance();
+    }
+
+    public DatabaseReference getReference(String path) {
+        return database.getReference(path);
+    }
+
+    public <T> void writeTo(String path, T value) {
+        database.getReference(path).setValue(value);
+    }
 
 }
