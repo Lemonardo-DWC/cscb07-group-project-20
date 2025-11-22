1046b197d0b510e9d52274c1744e065a45bcf78a
le <lemon.xu@mail.utoronto.ca>
2025-11-22 02:09:47 -0500

    fix: fixed bug where a child account is created even though username was already taken

---
+++ app/src/main/java/com/example/smartair/AddChildViewModelFactory.java
@@ -0,0 +1,27 @@
+package com.example.smartair;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.viewmodel.CreationExtras;
+
+public class AddChildViewModelFactory implements ViewModelProvider.Factory {
+
+    private final Context context;
+
+    public AddChildViewModelFactory(Context context) {
+        this.context = context.getApplicationContext();
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass,
+                                          @NonNull CreationExtras extras) {
+        if (modelClass.isAssignableFrom(AddChildViewModel.class)) {
+            return (T) new AddChildViewModel(context);
+        }
+        throw new IllegalArgumentException("Unknown ViewModel class");
+    }
+}
